7f86c75427f2320a39d023cbdc55179080d38f17
JUOSORIOT <jaot12345@gmail.com>
2021-10-12 16:56:49 -0500

    Lectura de datos desde el serial microcontrolador
    Se crea el buffer de entrada desde el puerto serial con los 10 bytes para proximamente pasar a un estado 2

--- Sensor/Sensor.ino
+++ Sensor/Sensor.ino
@@ -25,15 +25,52 @@ unsigned int uiCrc16Cal(unsigned char const *pucY, unsigned char ucX)
 
 
 void taskCom() {
+  static uint8_t state = 1;
   unsigned char ab[1]={0x38};
-  static uint8_t numero_1 = 0.0f;
+  static float numero_1 = 0.0;
+  static uint8_t arreglo_1[4] = {0};
+  static float numero_2 = 0.0;
+  static uint8_t arreglo_2[4] = {0};
+  static uint8_t bufferRx[10] = {0};
+  static uint8_t dataCounter = 0;
+  //Serial.write(uiCrc16Cal(ab,1));
+  switch (state) {
+  case 1:
+    if(Serial.available())
+    {
+      uint8_t dataRx = Serial.read();
+       
+      if (dataCounter >= 9) {
+          bufferRx[dataCounter] = dataRx;
+          dataCounter = 0;
+          for (int i = 0; i < 10; i++) 
+          {
+            Serial.write(bufferRx[i]);
+          }
+          Serial.write(uiCrc16Cal(bufferRx,9));
+          //state = 2;
+      }
+      else
+      {
+          bufferRx[dataCounter] = dataRx;
+          dataCounter++;              
+      } 
+      
+      
+      
+      
+      
+    }
+    break;
+  case 2:
+    //do something when var equals 2
+    break;
+  default:
+    // if nothing else matches, do the default
+    // default is optional
+    break;
+}
   
-  if(Serial.available())
-  {
-    Serial.read();
-
-    Serial.write(uiCrc16Cal(ab,1));
-  }
 }
 void setup() {
   Serial.begin(115200);
