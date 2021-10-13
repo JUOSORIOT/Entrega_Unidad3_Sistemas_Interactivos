2be354eaaf124e947be1809a6a2d92f4aa03c7e8
JUOSORIOT <jaot12345@gmail.com>
2021-10-13 18:06:01 -0500

    Ajuste de detalles finales
    Se corrigio el checksum para que funcionara y mostrara sus 2 bytes y se termino el codigo de la AI

--- Sensor/Sensor.ino
+++ Sensor/Sensor.ino
@@ -38,28 +38,34 @@ void taskCom() {
   static float numero_1 = 0.0;
   static float numero_2 = 0.0;
   static float resultado = 0.0;
-  static uint8_t arrayResultado[5] = {0};
-  static uint8_t bufferRx[10] = {0};
+  static uint8_t arrayResultado[6] = {0};
+  static uint8_t bufferRx[11] = {0};
   static uint8_t dataCounter = 0;
-  //Serial.write(uiCrc16Cal(ab,1));
+  static uint8_t arrayCheck[2] ={0};
+  static uint8_t arrayCheckr[2] ={0};  
+
   switch (state) {
   case 1:
     while(Serial.available())
     {
       uint8_t dataRx = Serial.read();
        
-      if (dataCounter >= 9) {
+      if (dataCounter >= 10) {
           bufferRx[dataCounter] = dataRx;
           dataCounter = 0;
-          for (int i = 0; i < 10; i++) 
+          for (int i = 0; i < 11; i++) 
           {
-            Serial.write(bufferRx[i]);
+            //Serial.write(bufferRx[i]);
           }
-          Serial.write(uiCrc16Cal(bufferRx,9));
-          if((char)bufferRx[9] == (char)uiCrc16Cal(bufferRx,9))
+          //Serial.write(uiCrc16Cal(bufferRx,9));
+          uint16_t checkp = uiCrc16Cal(bufferRx,9);
+          memcpy(arrayCheck,(uint8_t *)&checkp,2);
+          //Serial.write(arrayCheck,2);
+          if(arrayCheck[0]==bufferRx[9] and arrayCheck[1]==bufferRx[10])
           {
             state = 2;            
-          }          
+          }
+         
       }
       else
       {
@@ -71,9 +77,9 @@ void taskCom() {
   case 2:
     numero_1 = getFloat(bufferRx, 0);
     numero_2 = getFloat(bufferRx, 4);    
-    Serial.println(numero_1);
-    Serial.println(numero_2);
-    Serial.println((char)bufferRx[8]);
+    //Serial.println(numero_1);
+    //Serial.println(numero_2);
+    //Serial.println((char)bufferRx[8]);
     if(((char)bufferRx[8])== '+')
     {
       resultado = numero_1 + numero_2;
@@ -91,18 +97,22 @@ void taskCom() {
       resultado = numero_1 / numero_2;
     }
 
-    Serial.println(resultado);
-    Serial.println("Llegamos al estado 2, Felicidades");
+    //Serial.println(resultado);
+    //Serial.println("Llegamos al estado 2, Felicidades");
     state = 3;
     break;
   case 3:
     memcpy(arrayResultado, (uint8_t *)&resultado,4);
-    arrayResultado[4] = uiCrc16Cal(arrayResultado,4);
-    Serial.write(arrayResultado,5);
+    //arrayResultado[4] = uiCrc16Cal(arrayResultado,4);
+    uint16_t checkr = uiCrc16Cal(arrayResultado,4);
+    memcpy(arrayCheckr,(uint8_t *)&checkr,2);
+    arrayResultado[4] = arrayCheckr[0];
+    arrayResultado[5] = arrayCheckr[1];
+    Serial.write(arrayResultado,6);
     state = 1;
     break;
   default:
-    Serial.print("Si estas aqui posees problemas");
+    //Serial.print("Si estas aqui posees problemas");
     break;
   }  
 }
